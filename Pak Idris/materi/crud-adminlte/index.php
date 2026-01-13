25153688f9e1fd95a8970c9cd6784c54ba118715
AD_TECH <muhammadabidhafizalauddin6@gmail.com>
2026-01-13 10:26:53 +0700

    menambahkan file modifikasi adminlte

--- Pak Idris/materi/crud-adminlte/index.php
+++ Pak Idris/materi/crud-adminlte/index.php
@@ -11,14 +11,57 @@ include './template/header.php';
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard v1</li>
-                    </ol>
-                </div><!-- /.col -->
+                </div>
             </div><!-- /.row -->
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-info"><i class="fas fa-box"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Total Barang</span>
+                            <span class="info-box-number">1,410</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-success"><i class="fas fa-shopping-cart"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Total Stok</span>
+                            <span class="info-box-number">410</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+            </div><!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title m-0">Selamat Datang di aplikasi CRUD Barang</h5>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="card-title">Aplikasi ini digunakan untuk mengelola data barang dengan fitur CRUD (Create, Read, Update, Delete)</h6>
+
+                            <p class="card-text">Fitur yang tersedia:</p>
+                            <ul>
+                                <li>Menampilkan data barang</li>
+                                <li>Menambah data barang baru</li>
+                                <li>Mengedit data barang</li>
+                                <li>Menghapus data barang</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary">Lihat Data Barang</a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
