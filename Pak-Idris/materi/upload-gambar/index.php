1e0b543f7755822bebb018809a5b133d5438938e
AD-TECH75 <muhammadabidhafizalauddin6@gmail.com>
2026-03-10 07:41:26 +0700

    modify index and upload php files and add gallery.php files

--- Pak-Idris/materi/upload-gambar/index.php
+++ Pak-Idris/materi/upload-gambar/index.php
@@ -4,20 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Proses upload gambar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 
 <body class="p-4">
-    <h2 class="mb-4">upload file</h2>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <div>
-            <label for="gambar" class="form-label">upload file</label>
-            <input type="file" name="gambar" id="gambar" class="form-control mb-3">
-        </div>
-        <button type="submit">SUBMIT</button>
-    </form>
+    <div class="container">
+        <h2 class="text text-uppercase">upload gambar</h2>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="gambar" class="form-label">gambar</label>
+                <input type="file" name="gambar" id="gambar" class="form-control">
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">submit</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
