d02ec570cd8be1e0d3a7e8a246726afdd6775e22
AD_TECH <muhammadabidhafizalauddin6@gmail.com>
2026-04-20 10:55:57 +0700

    modified project-3 and modified another file

---
+++ laravel/project-3/resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+
+<body class="bg-black">
+    <header class="mt-3 sticky-top">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary rounded">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">
+                        <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                        Bootstrap
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav ms-auto">
+                            <a class="nav-link active" aria-current="page" href="/home">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
